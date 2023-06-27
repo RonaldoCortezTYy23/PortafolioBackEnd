771c0cf5c483c5e567bd2fdec6b8ad979a394a28
RonaldoCortezTYy23 <120589481+RonaldoCortezTYy23@users.noreply.github.com>
2023-06-27 20:29:02 -0300

    subiendo script y der de la base de datos

--- src/main/java/com/porfolio/mgb/Controller/CEducacion.java
+++ src/main/java/com/porfolio/mgb/Controller/CEducacion.java
@@ -26,7 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frondend-ronaldo-cortez.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+/*@CrossOrigin(origins = "https://frondend-ronaldo-cortez.web.app")*/
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/porfolio/mgb/Controller/CExperiencia.java
+++ src/main/java/com/porfolio/mgb/Controller/CExperiencia.java
@@ -28,7 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "https://frondend-ronaldo-cortez.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+/*@CrossOrigin(origins = "https://frondend-ronaldo-cortez.web.app")*/
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
